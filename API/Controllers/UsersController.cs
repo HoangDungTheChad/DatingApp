f1eae0afdc5dd10a6cc6fdbec25ea7f6de83a54b
HoangDungTheChad <hoangdungthechad@gmail.com>
2025-05-16 23:12:05 +0700

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,11 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers; 
-
-[ApiController] 
-[Route("api/[controller]")]
-public class UsersController : ControllerBase 
+public class UsersController : BaseApiController
 {
   private readonly DataContext _context; 
   public UsersController(DataContext context)
@@ -16,12 +14,14 @@ public class UsersController : ControllerBase
   }
 
   [HttpGet] 
+  [AllowAnonymous]
   public async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
   {
     return await _context.Users.ToListAsync(); 
   } 
 
   [HttpGet("{id}")]
+  [Authorize]
   public async Task<ActionResult<AppUser>> GetUser(int id) 
   {
     return await _context.Users.FindAsync(id); 
--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,20 @@
+using System.Text;
 using API.Data;
-using Microsoft.EntityFrameworkCore; 
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers(); 
-builder.Services.AddDbContext<DataContext>(options => {
-  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));; 
-}); 
+builder.Services.AddIdentityServices(builder.Configuration); 
+
 
 var app = builder.Build();
 
@@ -22,6 +28,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); 
+
+// the order of these middleware is important 
+app.UseAuthentication();  
+app.UseAuthorization(); 
 
 app.MapControllers(); 
 
